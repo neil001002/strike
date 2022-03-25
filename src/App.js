acf2d8980fd9f7d4b6f5194fa493de428b8dbbc6
sunil <neil001002@gmail.com>
2022-03-25 05:42:47 +0530

    login page design

--- src/App.js
+++ src/App.js
@@ -8,7 +8,38 @@ import { useContext } from "react";
 function App() {
   const { currentAccount } = useContext(LoginContext);
 
-  return <div className="App">{!currentAccount ? <Login /> : <Profiles />}</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <header>
+            <div className="left">
+              <p className="title">Strike</p>
+            </div>
+            {/* Display a logo and wallet connection status */}
+            <div className="right">
+              {currentAccount ? (
+                <p>
+                  {" "}
+                  {currentAccount.slice(0, 6)}...{currentAccount.slice(-4)}{" "}
+                </p>
+              ) : (
+                <p>Not connected</p>
+              )}
+            </div>
+          </header>
+        </div>
+
+        {!currentAccount ? <Login /> : <Profiles />}
+
+        {/* footer */}
+        <div className="footer-container">
+          <a className="footer-text">Lens Protocol</a>
+          <a className="footer-text">developed by sunil</a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -2,6 +2,7 @@ import "./Login.css";
 import { useQuery } from "@apollo/client";
 import React, { useContext, useEffect, useState } from "react";
 import { LoginContext } from "../../context/LoginContext";
+import lensBgLogo from "../../assets/lens-illustration_grow.svg";
 
 function Login() {
   const { connectWallet } = useContext(LoginContext);
@@ -9,10 +10,16 @@ function Login() {
   // console.log("Here's the challenge text: ", { error, loading, data });
 
   return (
-    <div className="Login">
-      <button onClick={connectWallet}>
-        <h1>Connect Wallet</h1>
-      </button>
+    <div className="login-page">
+      <div className="connect-wallet-container">
+        <div className="img-container">
+          <img className="bg-logo" src={lensBgLogo} />
+        </div>
+        <button className="cta-button connect-wallet-button" onClick={connectWallet}>
+          Connect Wallet
+        </button>
+      </div>
+      <p className="subtitle">The Last Social Media Handle You'll Ever Have To Create</p>
     </div>
   );
 }
